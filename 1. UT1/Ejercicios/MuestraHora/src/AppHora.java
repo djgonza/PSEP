a8693e6210dbfd93b683fe8f47a1a46ed6dbd6bf
djgonza <davidjimenezgonza@gmail.com>
2017-09-28 11:49:21 +0200

    UPDATE PSEP

--- 1. UT1/Ejercicios/MuestraHora/src/AppHora.java
+++ 1. UT1/Ejercicios/MuestraHora/src/AppHora.java
@@ -8,7 +8,7 @@ public class AppHora {
 		
 		HiloHora[] horas = new HiloHora[10];
 		
-		for (int i = 0; i<=10; i++) {
+		for (int i = 0; i<10; i++) {
 			horas[i] = new HiloHora();
 			horas[i].start();
 		}
--- 1. UT1/Ejercicios/MuestraHora/src/HiloHora.java
+++ 1. UT1/Ejercicios/MuestraHora/src/HiloHora.java
@@ -5,22 +5,22 @@ import java.util.Date;
 
 public class HiloHora extends Thread{
 			
-	public void run() {
-		
-		
+	public void run() {		
 		
 		for(int i=0;i <=5; i++) {			
 			
 			try {
-				System.out.print("Mensaje nº: ");
-				System.out.print(i);
-				System.out.print(" Nombre: ");
-				System.out.print(this.currentThread());
-				System.out.print(" Hora: ");
-				DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				String cadena = "";
+				cadena += "Mensaje nº: ";
+				cadena += i;
+				cadena += " Nombre: ";
+				cadena += this.currentThread();
+				cadena += " Hora: ";
+				DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 				Date date = new Date();
-				System.out.print(dateFormat.format(date));
-				System.out.println();
+				cadena += dateFormat.format(date);
+						
+				System.out.println(cadena + " Fin!");				
 				this.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
